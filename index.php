2f3a147d7e19531b3cc37889789829f31ff216b7
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-22 09:51:04 +0200

    exercice 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$object = 'stylo';
+$message = 'Salut';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8/">
+    <title>Exercice1</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+      echo $object,$message;
+      ?>
+    </h1>
+  </body>
+</html>
