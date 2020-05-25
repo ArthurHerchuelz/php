37e34c2dc0b7b570a0565635a1db3f50a80ba9af
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 14:00:47 +0200

    fini

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ $number = 140;
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>Exercice6</title>
-  </head>
-  <body>
-    <h1>
-      <?= ($number + 30)/2; ?>
-    </h1>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <title>Exercice 6</title>
+</head>
+<body>
+  <h1>
+    <?= ($number + 30)/2; ?>
+  </h1>
+</body>
 </html>
