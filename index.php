aec76d30635d6d18238359f43877eae47e465956
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 11:03:51 +0200

    exercice4partie3

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 $départements = array (
-'02' => 'Aisne'
-'59' => 'Nord'
-'60' => 'Oise'
-'62' => 'Pas-de-Calais'
+'02' => 'Aisne',
+'59' => 'Nord',
+'60' => 'Oise',
+'62' => 'Pas-de-Calais',
 '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>Exercice 3 partie 3</title>
+  <title>Exercice 4 partie 3</title>
 </head>
 <body>
   <p></p>
