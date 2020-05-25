2c39109bc7975faed3cd082364c926cd7685458e
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 09:23:02 +0200

    fini

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
-$number = 140;
+$age = 28;
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8/">
-    <title>Exercice5</title>
-  </head>
-  <body>
-    <h1>
-      <?php
-      echo ($number + 30)/2;
-      ?>
-    </h1>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <title>Exercice5</title>
+</head>
+<body>
+  <h1>
+    <?php
+    if ($age >= 18){
+      echo 'vous êtes majeur';
+    }
+    else{
+      echo 'vous êtes mineur';
+    }
+    ?>
+  </h1>
+</body>
 </html>
