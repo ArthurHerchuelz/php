2531b17e36f6f69b4f450cb9f717f62d6c3e3a2c
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 16:12:37 +0200

    fini

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
-$firstString = 'bonjour';
-$secondString = 'Arthur';
+$name= 'Sidonie';
+$name2 = 'Fabrice';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>Exercice 1 partie 4</title>
-  </head>
-  <body>
-    <h1>
-      <?php
-      function hello($firstString, $secondString)
-      {
-        $hello = $firstString .' '. $secondString;
-        return $hello;
-      }
-        echo hello();
-      ?>
-    </h1>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <title>Exercice 1 partie 4</title>
+</head>
+<body>
+  <h1>
+    <?php
+    function DireBonjour($name, $name2)
+    {
+      $hello = 'Bonjour ' . $name . ' et ' . $name2 . ' !';
+      return $hello;
+    }
+    echo DireBonjour($name, $name2);
+    ?>
+  </h1>
+</body>
 </html>
