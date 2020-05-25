df41f0a81311a38af49695982fc26a9aa41d1788
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 10:33:07 +0200

    exercice1partie3

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
-$age = 28;
+$month = array ('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>Exercice 3 partie 2</title>
+  <title>Exercice 1 partie 3</title>
 </head>
 <body>
-  <h1>
-    <?php
-    if ($age >= 18){
-      echo 'Tu es majeur';
-    }
-    else {
-      echo 'Tu n\'es pas majeur';
-    }
-    ?>
-  </h1>
 </body>
 </html>
