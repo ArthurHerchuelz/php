ef5836ff0b6d471120e77c8b9932b2ee5dd1c47f
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 13:58:04 +0200

    exercice 6

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $number = 140;
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8/">
-    <title>Exercice5</title>
+    <meta charset="utf-8"/>
+    <title>Exercice6</title>
   </head>
   <body>
     <h1>
