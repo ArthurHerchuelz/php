ac7c9ed51e2768cb1cf88b7ee8084830f6a9aaf6
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-25 15:08:51 +0200

    exercice 1 partie 4

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-$string = str_shuffle('bonjour');
+$name = 'Arthur';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>Exercice 1 partie 4</title>
-  </head>
-  <body>
-    <h1>
-      <?= $string; ?>
-    </h1>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <title>Exercice 1 partie 4</title>
+</head>
+<body>
+  <h1>
+    <?php
+    function DireBonjour($name)
+    {
+      return $name;
+    }
+    echo 'Bonjour ' . DireBonjour($name) ;
+    ?>
+  </h1>
+</body>
 </html>
