61f76e2953f8d1a49964bb8fe9f85d56e7236dec
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-26 10:14:45 +0200

    exercice6partie5

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
-$counter = 0;
+$counter = 1;
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>Exercice 3 partie 3</title>
+  <title>Exercice 6 partie 5</title>
 </head>
 <body>
   <p>
     <?php
-    while ($counter < 300) {
-      if ($counter%2){
+    while ($counter <= 100) {
+      if ($counter%3 == 0 && $counter%5 <> 0){
+        echo 'Fizz <br />';
+        $counter += 1;
+      } else if ($counter%5 == 0 && $counter%3 <> 0){
+        echo 'Buzz <br />';
+        $counter += 1;
+      } else if ($counter%3 == 0 && $counter%5 == 0 ){
+        echo 'FizzBuzz <br />';
         $counter += 1;
       } else {
+        echo $counter .'<br />';
         $counter += 1;
-        echo $counter . '<br />';
       }
     }
     ?>
