ea12d1eb7395ea4cb3a001de50470c2712ee9081
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-27 10:29:37 +0200

    CORRECTION

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <?php
 $départements = array (
-'02' => 'Aisne',
-'59' => 'Nord',
-'60' => 'Oise',
-'62' => 'Pas-de-Calais',
-'80' => 'Somme');
-?>
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-<head>
-  <meta charset="utf-8"/>
-  <title>Exercice 5 partie 3</title>
-</head>
-<body>
-  <p><?php
-  foreach($départements as $cle => $element)
-{
-    echo "Le département $element a le numéro $cle <br />";
-} ?></p>
-</body>
-</html>
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme');
+  ?>
+  <!DOCTYPE html>
+  <html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Exercice 5 partie 3</title>
+  </head>
+  <body>
+    <ul>
+      <?php
+      foreach($départements as $cle => $element)
+      { ?>
+        <li>
+        <?php
+          echo "Le département $element a le numéro $cle";
+        ?>
+        </li>
+        <?php } ?>
+    </ul>
+  </body>
+  </html>
